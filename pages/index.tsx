4f97c5ff20d9a6e2355ba796dafde739781119ef
MinCheolS <ay5212@hanmail.net>
2024-04-15 00:40:04 +0900

    chore: 초기셋팅

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam id
+        voluptas laboriosam ipsam eum obcaecati tenetur. Distinctio ipsa enim
+        cupiditate cum illo eius inventore tenetur, et, delectus quidem ex
+        fugiat.
+      </span>
+    </div>
+  );
+}
+
+export default Home;
