c2ee483c806666e274337bf474fcdf1ef1b948c2
Jules <dali1999@naver.com>
2024-04-15 16:24:30 +0900

    feat: media query breakpoints 추가

---
+++ utils/breakpointsDevice.ts
@@ -0,0 +1,8 @@
+import size from '@constants/breakpointsSize';
+
+const device = {
+  mobile: `(max-width: ${size.mobile}px)`,
+  tablet: `(max-width: ${size.tablet}px)`,
+};
+
+export default device;
