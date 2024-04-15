f1fceed1ff4de848c373b9c45654deb8e28bfc48
김동현 <dtc03003@naver.com>
2024-04-15 21:51:41 +0900

    feat: DashBoardHeader 컴포넌트 구현

---
+++ components/headers/DashBoardHeader.style.ts
@@ -0,0 +1,120 @@
+import device from '@utils/breakpointsDevice';
+import styled from 'styled-components';
+import { Header } from './Header.style';
+
+export const DashBoardHeader = styled(Header)`
+  @media ${device.tablet} {
+    justify-content: flex-end;
+  }
+
+  @media ${device.mobile} {
+    padding-right: 12px;
+  }
+`;
+
+export const MyDashBoard = styled.div`
+  margin-left: calc(340px - 26.2px);
+  color: var(--black200);
+  font-size: 20px;
+  font-weight: 700;
+  line-height: normal;
+  white-space: nowrap;
+
+  @media ${device.tablet} {
+    display: none;
+  }
+`;
+
+export const ManagementContainer = styled.div`
+  display: flex;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  gap: 16px;
+
+  @media ${device.mobile} {
+    gap: 6px;
+  }
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 10px 16px;
+  gap: 8px;
+  flex-wrap: nowrap;
+  white-space: nowrap;
+  border-radius: 8px;
+  border: 1px solid var(--gray300);
+  background: var(--white);
+
+  color: var(--gray500);
+  font-size: 16px;
+  font-weight: 500;
+  line-height: normal;
+
+  @media ${device.mobile} {
+    padding: 6px 12px;
+    font-size: 14px;
+  }
+`;
+
+export const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+
+  @media ${device.mobile} {
+    display: none;
+  }
+`;
+
+export const Line = styled.div`
+  height: 38px;
+  border-left: 1px solid var(--gray300);
+  margin: 0 24px 0 32px;
+
+  @media ${device.mobile} {
+    height: 34px;
+    margin: 0 12px 0 16px;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const ProfileIcon = styled.div`
+  width: 38px;
+  height: 38px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  background-color: #a3c4a2;
+  color: var(--white);
+  text-align: center;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: normal;
+
+  @media ${device.mobile} {
+    width: 34px;
+    height: 34px;
+    font-size: 14px;
+  }
+`;
+
+export const ProfileName = styled.div`
+  color: var(--black200);
+  font-size: 16px;
+  font-weight: 500;
+  line-height: normal;
+  white-space: nowrap;
+
+  @media ${device.mobile} {
+    display: none;
+  }
+`;
---
+++ components/headers/DashBoardHeader.tsx
@@ -0,0 +1,31 @@
+import Header from './Header';
+import * as S from './DashBoardHeader.style';
+import setting from '../../public/icons/setting.svg';
+import add from '../../public/icons/add.svg';
+
+const DashBoardHeader = () => {
+  return (
+    <S.DashBoardHeader>
+      <S.MyDashBoard>내 대시보드</S.MyDashBoard>
+      <S.ManagementContainer>
+        <S.Buttons>
+          <S.Button>
+            <S.Icon src={setting.src} alt="관리 아이콘" />
+            관리
+          </S.Button>
+          <S.Button>
+            <S.Icon src={add.src} alt="추가 아이콘" />
+            초대하기
+          </S.Button>
+        </S.Buttons>
+        <S.Line />
+        <S.Profile>
+          <S.ProfileIcon>B</S.ProfileIcon>
+          <S.ProfileName>배유철</S.ProfileName>
+        </S.Profile>
+      </S.ManagementContainer>
+    </S.DashBoardHeader>
+  );
+};
+
+export default DashBoardHeader;
