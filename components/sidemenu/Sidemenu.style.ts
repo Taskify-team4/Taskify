1ca178beeffb29e6c40fcae7b7622fe8dd8f1b40
hong <hsw5971@naver.com>
2024-04-15 22:46:20 +0900

    feat: sidemenu 내에 임시 dashboard 생성 및 UI 구현

--- components/sidemenu/Sidemenu.style.ts
+++ components/sidemenu/Sidemenu.style.ts
@@ -1,54 +1,116 @@
 import styled from 'styled-components';
+import device from '@utils/breakpointsDevice';
 import Image from 'next/image';
+
+// 임시로 만듦.
+export const TempChip = styled.span`
+  width: 8px;
+  height: 8px;
+  background-color: #7ac555;
+  border-radius: 4px;
+`;
 
 export const SidemenuContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: fit-content;
-  padding: 20px 20px 0 20px;
+  height: 100vh;
+  padding: 0 12px;
+  border-right: 1px solid var(--gray300);
+  @media ${device.mobile} {
+    align-items: center;
+  }
 `;
 export const logoWrapper = styled.div`
   display: flex;
   align-items: baseline;
-  padding-bottom: 40px;
-  @media (min-width: 768px) {
-    padding-bottom: 60px;
+  padding: 20px 8px 60px 8px;
+  @media ${device.mobile} {
+    padding-bottom: 40px;
   }
 `;
 export const logoImg = styled(Image)`
-  width: 24px;
-  height: 28px;
-  @media (min-width: 768px) {
-    width: 28px;
-    height: 32px;
+  width: 28px;
+  height: 32px;
+  @media ${device.mobile} {
+    width: 24px;
+    height: 28px;
   }
 `;
 export const logoImgTaskify = styled(Image)`
   width: 80px;
   height: 22px;
-  display: none;
-  @media (min-width: 768px) {
-    display: inline;
+  @media ${device.mobile} {
+    display: none;
   }
 `;
 export const dashBoardsWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  @media (min-width: 1280px) {
-    gap: 160px;
+  padding: 12px 12px 16px 12px;
+  gap: 160px;
+  cursor: pointer;
+  @media ${device.tablet} {
+    gap: 0px;
   }
 `;
 export const dashBoardsText = styled.div`
-  display: none;
   color: var(--gray500);
   font-size: 12px;
   font-weight: 700;
-  @media (min-width: 768px) {
-    display: inline;
+  @media ${device.mobile} {
+    display: none;
   }
 `;
 export const addBoxImg = styled(Image)`
   width: 20px;
   height: 20px;
+`;
+export const dashBoardsChips = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  overflow-y: scroll;
+  scrollbar-width: thin;
+  @media ${device.mobile} {
+    scrollbar-width: none;
+  }
+`;
+export const dashBoardsChip = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  padding: 12px;
+  border-radius: 4px;
+  cursor: pointer;
+  #title {
+    color: var(--gray500);
+    font-size: 18px;
+    font-weight: 500;
+    max-width: 196px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  &:hover {
+    background-color: var(--violetlight);
+  }
+  @media ${device.tablet} {
+    #title {
+      max-width: 68px;
+    }
+  }
+  @media ${device.mobile} {
+    #title {
+      display: none;
+    }
+  }
+`;
+export const dashBoardCrown = styled(Image)`
+  width: 15px;
+  height: 12px;
+  @media ${device.mobile} {
+    display: none;
+  }
 `;
--- components/sidemenu/Sidemenu.tsx
+++ components/sidemenu/Sidemenu.tsx
@@ -2,8 +2,19 @@ import * as S from '@components/sidemenu/Sidemenu.style';
 import logoImg from '@public/icons/logo_img.svg';
 import logoImgTaskify from '@public/icons/logo_taskify.svg';
 import addBoxImg from '@public/icons/add.svg';
+import crownImg from '@public/icons/crown.svg';
 
-export default function Sidemenu() {
+// 임시로 만듦.
+type dashboard = {
+  color: string;
+  title: string;
+  crown?: boolean;
+};
+
+type SidemenuProps = {
+  dashboards: dashboard[];
+};
+export default function Sidemenu({ dashboards }: SidemenuProps) {
   return (
     <S.SidemenuContainer>
       <S.logoWrapper>
@@ -14,6 +25,21 @@ export default function Sidemenu() {
         <S.dashBoardsText>Dash Boards</S.dashBoardsText>
         <S.addBoxImg src={addBoxImg} alt="add box" />
       </S.dashBoardsWrapper>
+      <S.dashBoardsChips>
+        {dashboards &&
+          dashboards.map((dashboard, index) => (
+            <S.dashBoardsChip key={`${index} ${dashboard.title}`}>
+              <S.TempChip></S.TempChip>
+              <span id="title">{dashboard.title}</span>
+              {dashboard.crown && (
+                <S.dashBoardCrown
+                  src={crownImg}
+                  alt="본인이 만든 대시보드일 때 생기는 왕관 아이콘"
+                />
+              )}
+            </S.dashBoardsChip>
+          ))}
+      </S.dashBoardsChips>
     </S.SidemenuContainer>
   );
 }
