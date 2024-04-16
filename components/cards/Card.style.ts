da69c9efebc714feef628c97522114205f76fe43
yohan11 <yipco77@naver.com>
2024-04-16 14:33:04 +0900

    feat: 반응형 디자인 적용

--- components/cards/Card.style.ts
+++ components/cards/Card.style.ts
@@ -10,6 +10,15 @@ export const CardContainer = styled.div`
 
   border: var(--gray300) 1px solid;
   border-radius: 6px;
+
+  @media ${device.tablet} {
+    flex-direction: row;
+  }
+
+  @media ${device.mobile} {
+    flex-direction: column;
+    padding: 10px;
+  }
 `;
 
 export const CardImage = styled.div`
@@ -17,7 +26,7 @@ export const CardImage = styled.div`
 
   width: 270px;
   height: 160px;
-  
+
   @media ${device.tablet} {
     width: 91px;
     height: 53px;
@@ -28,11 +37,27 @@ export const CardImage = styled.div`
   }
 `;
 
+export const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  gap: 12px;
+`;
+
 export const CardTitle = styled.div`
   font-size: 16px;
   font-weight: 500;
   @media ${device.tablet} {
     font-size: 14px;
+  }
+`;
+
+export const CardMeta = styled.div`
+  @media ${device.tablet} {
+    display: flex;
+  }
+  @media ${device.mobile} {
+    display: block;
   }
 `;
 
@@ -45,12 +70,20 @@ export const CardDate = styled.div`
   display: flex;
   gap: 6px;
   align-items: center;
+  margin-top: 12px;
 
   font-weight: 500;
   font-size: 12px;
   color: var(--gray500);
 
+  @media ${device.tablet} {
+    margin-left: 10px;
+    margin-top: 0;
+  }
+
   @media ${device.mobile} {
     font-size: 10px;
+    margin-left: 0;
+    margin-top: 12px;
   }
 `;
--- components/cards/Card.tsx
+++ components/cards/Card.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import * as S from '@components/cards/Card.style';
 import { SquareChip } from '@components/chip/Chip.style';
-import calendarIcon from '@public/icons/calendar.svg';
 import Image from 'next/image';
+
+// 리소스
+import calendarIcon from '@public/icons/calendar.svg';
 import sampleCardImg from '@public/images/img1.png';
 
 function Card() {
@@ -11,18 +13,22 @@ function Card() {
       <S.CardImage>
         <Image src={sampleCardImg.src} alt={'sample'} fill />
       </S.CardImage>
-      <S.CardTitle>새로운 일정 관리 Taskify</S.CardTitle>
-      <S.CardChips>
-        <SquareChip $size={'small'} $color={'orange'}>
-          프로젝트
-        </SquareChip>
-        <SquareChip $size={'small'} $color={'pink'}>
-          백엔드
-        </SquareChip>
-      </S.CardChips>
-      <S.CardDate>
-        <Image src={calendarIcon.src} alt={'calendarIcon'} width={18} height={18} /> 2024.12.12
-      </S.CardDate>
+      <S.CardContent>
+        <S.CardTitle>새로운 일정 관리 Taskify</S.CardTitle>
+        <S.CardMeta>
+          <S.CardChips>
+            <SquareChip $size={'small'} $color={'orange'}>
+              프로젝트
+            </SquareChip>
+            <SquareChip $size={'small'} $color={'pink'}>
+              백엔드
+            </SquareChip>
+          </S.CardChips>
+          <S.CardDate>
+            <Image src={calendarIcon.src} alt={'calendarIcon'} width={18} height={18} /> 2024.12.12
+          </S.CardDate>
+        </S.CardMeta>
+      </S.CardContent>
     </S.CardContainer>
   );
 }
