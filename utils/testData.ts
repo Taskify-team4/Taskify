d5102855e06261091e1cafd1cdf66953c93b464f
dali1999 <dali1999@naver.com>
2024-04-17 02:04:07 +0900

    feat: 샘플 데이터 추가

---
+++ utils/testData.ts
@@ -0,0 +1,24 @@
+import { User } from '@components/table/Table.type';
+
+export const USER_LIST: User[] = [
+  {
+    nickname: '정만철',
+    email: 'codeitA@codeit.com',
+    id: 1,
+  },
+  {
+    nickname: '김태순',
+    email: 'codeitB@codeit.com',
+    id: 2,
+  },
+  {
+    nickname: '최주협',
+    email: 'codeitC@codeit.com',
+    id: 3,
+  },
+  {
+    nickname: '윤지현',
+    email: 'codeitD@codeit.com',
+    id: 4,
+  },
+];
