8c76214e0fff3913ee945b8374ab32b2db4df25a
yohan11 <yipco77@naver.com>
2024-04-16 12:38:48 +0900

    style: 컴포넌트 함수 스타일 변경

--- components/chip/Chip.tsx
+++ components/chip/Chip.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import * as S from './Chip.style';
 import { TChipProps } from '@components/chip/Chip.type';
 
-const Chip = ({ children, size, color }: TChipProps) => {
+function Chip({ children, size, color }: TChipProps) {
   return (
     <S.BasicChip $size={size} $color={color}>
       {children}
     </S.BasicChip>
   );
-};
+}
 
 export default Chip;
