88d6eab149e4c48818fe13cee18e70a4292b3392
Jules <dali1999@naver.com>
2024-04-17 15:23:57 +0900

    feat: createPortal 세팅

---
+++ components/modals/Modal.tsx
@@ -0,0 +1,74 @@
+import React, { ReactNode, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+import styled from 'styled-components';
+
+const BackdropContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.4);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+  color: var(--black300);
+  border-radius: 8px;
+  font-size: 30px;
+  padding: 28px;
+`;
+
+interface PortalProps {
+  isOpen: boolean;
+  children: ReactNode;
+}
+
+function Portal({ isOpen, children }: PortalProps) {
+  const [portal, setPortal] = useState<HTMLElement | null>();
+
+  useEffect(() => {
+    if (document) {
+      const portalElem = document.getElementById('portal');
+      setPortal(portalElem);
+    }
+  }, []);
+
+  if (!isOpen) {
+    return null;
+  }
+  return createPortal(children, portal as HTMLElement);
+}
+
+interface ModalProps {
+  children: ReactNode;
+  content: JSX.Element;
+  isConfirm?: boolean;
+  initialOpen?: boolean;
+}
+
+function Modal({ children, content, isConfirm = false, initialOpen = false }: ModalProps) {
+  const [open, setOpen] = useState<boolean>(initialOpen);
+
+  const modalHandler = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const Content = () => {
+    return React.cloneElement(content, { close: modalHandler });
+  };
+
+  return (
+    <div>
+      <div onClick={modalHandler}>{children}</div>
+      <Portal isOpen={open}>
+        <BackdropContainer
+          onClick={() => {
+            !isConfirm && modalHandler();
+          }}
+        />
+        <Content />
+      </Portal>
+    </div>
+  );
+}
+
+export default Modal;
