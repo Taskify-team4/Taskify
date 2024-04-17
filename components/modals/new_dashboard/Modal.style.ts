1838a49f1acc19e06f05abf0b562f8fc74525199
yohan11 <yipco77@naver.com>
2024-04-17 15:32:05 +0900

    design: 모달 내용만 꺼내서 쓸 수 있도록 디자인 수정

--- components/modals/new_dashboard/Modal.style.ts
+++ components/modals/new_dashboard/Modal.style.ts
@@ -5,13 +5,11 @@ export const ModalContainer = styled.div`
   max-width: 540px;
   height: 334px;
 
-  padding: 40px;
   position: relative;
 
   display: flex;
   flex-direction: column;
   gap: 30px;
-  box-sizing: border-box;
 
   background-color: white;
   border: 1px solid gray; // 확인 용도로 추가한 코드입니다.
@@ -20,7 +18,7 @@ export const ModalContainer = styled.div`
   @media ${device.mobile} {
     width: 327px;
     height: 293px;
-    padding: 20px;
+
     justify-content: center;
   }
 `;
@@ -38,17 +36,12 @@ export const ModalButtons = styled.div`
   display: flex;
   gap: 12px;
 
-  padding: 10px;
   position: absolute;
-  bottom: 10px;
-  right: 40px;
-
-  box-sizing: border-box;
+  bottom: 0;
+  right: 0;
 
   @media ${device.mobile} {
     position: static;
     justify-content: space-between;
-    padding: 0;
-    gap: 0;
   }
 `;
