8f243712d109bf76823e8c0ab249c2a122e7964a
yohan11 <yipco77@naver.com>
2024-04-18 21:58:46 +0900

    feat: 헤더, 사이드메뉴 데이터 api 연결

--- utils/editDashboard/api.ts
+++ utils/editDashboard/api.ts
@@ -1,6 +1,7 @@
 import baseAxios from '@node_modules/axios';
 import { User } from '@utils/testData';
 import { DashBoardMember, DashBoardNameData } from '@utils/editDashboard/edit.type';
+import { dashboard } from '@components/sidemenu/Sidemenu.type';
 
 const axios = baseAxios.create({
   baseURL: 'https://sp-taskify-api.vercel.app/4-4/',
@@ -54,6 +55,17 @@ export const getDashboardMembers = async (id: number): Promise<DashBoardMember[]
   return newMembers;
 };
 
+export const getMyData = async (): Promise<DashBoardMember[]> => {
+  const res = await axios.get(`users/me`, {
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTczMiwidGVhbUlkIjoiNC00IiwiaWF0IjoxNzEzNDI5OTU1LCJpc3MiOiJzcC10YXNraWZ5In0.1la3IrwbTBb9QjVdSl-1YpLnr64Fq74XXQpa_tqQp0A',
+    },
+  });
+
+  return res.data;
+};
+
 export const postDashboardInvites = async () => {
   const res = await axios.post(`dashboards/${6029}/invitations`, JSON.stringify({ email: 'asdf1234@naver.com' }), {
     headers: {
@@ -72,4 +84,15 @@ export const getDashboardCreate = async () => {
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTczMiwidGVhbUlkIjoiNC00IiwiaWF0IjoxNzEzNDI5OTU1LCJpc3MiOiJzcC10YXNraWZ5In0.1la3IrwbTBb9QjVdSl-1YpLnr64Fq74XXQpa_tqQp0A',
     },
   });
+};
+
+export const getDashboardList = async (): Promise<dashboard[]> => {
+  const res = await axios.get(`dashboards?navigationMethod=infiniteScroll`, {
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTczMiwidGVhbUlkIjoiNC00IiwiaWF0IjoxNzEzNDI5OTU1LCJpc3MiOiJzcC10YXNraWZ5In0.1la3IrwbTBb9QjVdSl-1YpLnr64Fq74XXQpa_tqQp0A',
+    },
+  });
+
+  return res.data.dashboards;
 };
