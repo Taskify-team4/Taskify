cc8185b9e36f3a11850771bf278e532e744d1e69
yohan11 <yipco77@naver.com>
2024-04-19 15:13:39 +0900

    feat: 멤버별 프로필 아이콘 구현

--- components/table/member/MemberList.tsx
+++ components/table/member/MemberList.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import * as S from '@components/table/TableList.style';
 import Button from '@components/buttons/Button';
 import { DataListProps } from '@components/table/Table.type';
+import Image from 'next/image';
+import ProfileIcon from '@components/profileIcon/ProfileIcon';
 
 function MemberList({ data, buttonText, onDeleteClick }: DataListProps) {
   return (
@@ -9,7 +11,13 @@ function MemberList({ data, buttonText, onDeleteClick }: DataListProps) {
       {data.map((item) => (
         <S.TableList key={item.id}>
           <S.MemberDataContainer>
-            <S.MemberProfile>{item.nickname[0]}</S.MemberProfile>
+            {item.profileImageUrl ? (
+              <S.MemberProfile>
+                <Image src={item.profileImageUrl} fill alt="유저 이미지" />
+              </S.MemberProfile>
+            ) : (
+              <ProfileIcon str={item.nickname[0]} />
+            )}
             <S.ListData>{item.nickname}</S.ListData>
           </S.MemberDataContainer>
           {!item.isOwner ? <Button.Delete onClick={() => onDeleteClick(item.id)}>{buttonText}</Button.Delete> : <></>}
