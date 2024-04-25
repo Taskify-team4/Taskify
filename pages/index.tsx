753788fb1302cb02a0b6fc1c7521c64a01da0725
MinCheolS <ay5212@hanmail.net>
2024-04-25 23:15:32 +0900

    feat: landing page에서 token이 존재할 때 리다이렉트 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import * as S from './landing.style';
+import { useRouter } from 'next/router';
+import * as S from '../components/pages/landing/landing.style';
 import MainHeader from '../components/headers/MainHeader';
 import LandingMain from '@components/pages/landing/LandingMain';
 import LandingBody from '@components/pages/landing/LandingBody';
@@ -8,6 +9,13 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 function landing() {
+  const router = useRouter();
+  const accessToken = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null;
+
+  if (accessToken) {
+    router.push('/mydashboard');
+  }
+
   useEffect(() => {
     AOS.init();
   }, []);
