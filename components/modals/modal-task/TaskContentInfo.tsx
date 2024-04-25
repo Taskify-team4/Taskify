ed9a6ecbdd972331f92c08b2bb19cbcb6254c766
dali1999 <dali1999@naver.com>
2024-04-25 20:44:35 +0900

    feat: Todo 이미지 업로드 로직 구현 및 카드에 적용

--- components/modals/modal-task/TaskContentInfo.tsx
+++ components/modals/modal-task/TaskContentInfo.tsx
@@ -2,16 +2,22 @@ import * as S from '@components/modals/modal-task/ModalTask.style';
 import TaskChips from './TaskChips';
 import TaskDeadline from './TaskDeadline';
 import { ModalTaskProps } from './ModalTask';
+import Image from 'next/image';
 
 type TaskContentInfoProps = Pick<ModalTaskProps, 'description' | 'tags' | 'dueDate' | 'assignee'>;
 
-function TaskContentInfo({ description, tags, dueDate, assignee, columntitle }: TaskContentInfoProps) {
+function TaskContentInfo({ imageUrl, description, tags, dueDate, assignee, columntitle }: TaskContentInfoProps) {
   return (
     <S.TaskContentInfo>
       <TaskDeadline {...{ dueDate, assignee }} />
       <S.InfoLeft>
         <TaskChips {...{ tags }} columntitle={columntitle} />
         <S.TaskExplanation>{description}</S.TaskExplanation>
+        {imageUrl ? (
+          <S.CardImage>
+            <Image src={imageUrl} alt="Card Image" width={0} height={0} layout="responsive" />
+          </S.CardImage>
+        ) : null}
       </S.InfoLeft>
     </S.TaskContentInfo>
   );
